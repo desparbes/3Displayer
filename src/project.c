a712ecbd983a832bbc020e88844182aa7f9a1cac
desparbes <desparbes@enseirb-matmeca.fr>
2015-06-20 15:01:29 +0200

    Factorisation du code du module project

--- src/project.c
+++ src/project.c
@@ -7,6 +7,13 @@
 #include "color.h"
 #include "scene.h"
 
+typedef struct {
+    Point *A, *B, *C;
+    Point *normalA, *normalB, *normalC;
+    Texture *textureA, *textureB, textureC;
+    SDL_Surface *texture;
+} Triangle;
+
 // return the vector between camera.O and the intersection of
 // (AB) and the NEARPLAN
 void projectPoint(const Point *A, const Point *B, Point *S)
@@ -84,10 +91,141 @@ void projectSegment(const Point *A, const Point *B, const Color *color)
     drawSegment(&t, &u, depthA, depthB, color);
 }
 
+static void cutInOneTriangle(const Point *A, const Point *B, const Point *C,
+			     float depthA, float depthB, float depthC,
+			     const Point *OA,
+			     SDL_Surface *triangle,
+			     const Texture *U, 
+			     const Texture *V, 
+			     const Texture *W,
+			     const Point *normalA, 
+			     const Point *normalB,
+			     const Point *normalC)
+{
+    float nearplan = getNearplan();
+    Frame *camera = getCamera();
+    Point AB, AC;
+    Point OpB, OpC;
+
+    diffPoint(B, A, &AB);
+    diffPoint(C, A, &AC);
+    
+    float kB = (nearplan - depthA) / scalarProduct(&camera->j, &AB);
+    float kC = (nearplan - depthA) / scalarProduct(&camera->j, &AC);
+    
+    setPoint(&OpB, 
+	     A->x + kB * AB.x - camera->O.x,
+	     A->y + kB * AB.y - camera->O.y,
+	     A->z + kB * AB.z - camera->O.z);
+    setPoint(&OpC,
+	     A->x + kC * AC.x - camera->O.x,
+	     A->y + kC * AC.y - camera->O.y,
+	     A->z + kC * AC.z - camera->O.z);
+    
+    Coord a, bn, cn;
+    projectCoord(OA, depthA, &a);
+    projectCoord(&OpB, nearplan, &bn);
+    projectCoord(&OpC, nearplan, &cn);
+    
+    Texture UV, UW;
+    setTexture(&UV,
+	       (V->x - U->x) * kB + U->x,
+	       (V->y - U->y) * kB + U->y);
+    setTexture(&UW,
+	       (W->x - U->x) * kC + U->x,
+	       (W->y - U->y) * kC + U->y);
+    
+    Point nAB, nAC;
+    setPoint(&nAB,
+	     (normalB->x - normalA->x) * kB + normalA->x,
+	     (normalB->y - normalA->y) * kB + normalA->y,
+	     (normalB->z - normalA->z) * kB + normalA->z);
+    setPoint(&nAC,
+	     (normalC->x - normalA->x) * kC + normalA->x,
+	     (normalC->y - normalA->y) * kC + normalA->y,
+	     (normalC->z - normalA->z) * kC + normalA->z);
+    
+    drawTriangle(&a, &bn, &cn,
+		 depthA,
+		 (depthB - depthA) * kB + depthA,
+		 (depthC - depthA) * kC + depthA,
+		 triangle,
+		 U, &UV, &UW,
+		 normalA, &nAB, &nAC);
+}
+
+static void cutInTwoTriangles(const Point *A, const Point *B, const Point *C,
+			      float depthA, float depthB, float depthC,
+			      const Point *OB, const Point *OC,
+			      SDL_Surface *triangle,
+			      const Texture *U, 
+			      const Texture *V, 
+			      const Texture *W,
+			      const Point *normalA, 
+			      const Point *normalB,
+			      const Point *normalC)
+{
+    float nearplan = getNearplan();
+    Frame *camera = getCamera();
+    Point AB, AC;
+
+    diffPoint(B, A, &AB);
+    diffPoint(C, A, &AC);
+    
+    float kB = (nearplan - depthA) / scalarProduct(&camera->j, &AB);
+    float kC = (nearplan - depthA) / scalarProduct(&camera->j, &AC);
+    
+    Point OpB, OpC;
+    setPoint(&OpB,
+	     A->x + kB * AB.x - camera->O.x,
+	     A->y + kB * AB.y - camera->O.y,
+	     A->z + kB * AB.z - camera->O.z);
+    setPoint(&OpC,
+	     A->x + kC * AC.x - camera->O.x,
+	     A->y + kC * AC.y - camera->O.y,
+	     A->z + kC * AC.z - camera->O.z);
+    
+    Coord b, c, opbn, opcn;
+    projectCoord(OB, depthB, &b);
+    projectCoord(OC, depthC, &c);
+    projectCoord(&OpB, nearplan, &opbn);
+    projectCoord(&OpC, nearplan, &opcn);
+    
+    Texture VU, WU;
+    setTexture(&VU, (V->x - U->x) * kB + U->x, (V->y - U->y) * kB + U->y);
+    setTexture(&WU, (W->x - U->x) * kC + U->x, (W->y - U->y) * kC + U->y);
+    
+    Point nAB, nAC;
+    setPoint(&nAB,
+	     (normalB->x - normalA->x) * kB + normalA->x,
+	     (normalB->y - normalA->y) * kB + normalA->y,
+	     (normalB->z - normalA->z) * kB + normalA->z);
+    setPoint(&nAC,
+	     (normalC->x - normalA->x) * kC + normalA->x,
+	     (normalC->y - normalA->y) * kC + normalA->y,
+	     (normalC->z - normalA->z) * kC + normalA->z);
+    
+    drawTriangle(&opbn, &c, &opcn,
+		 (depthB - depthA) * kB + depthA,
+		 depthC,
+		 (depthC - depthA) * kC + depthA,
+		 triangle,
+		 &VU, W, &WU,
+		 &nAB, normalC, &nAC);
+    
+    drawTriangle(&opbn, &b, &c,
+		 (depthB - depthA) * kB + depthA,
+		 depthB, depthC,
+		 triangle,
+		 &VU, V, W,
+		 &nAB, normalB, normalC);
+}
+
 void projectTriangle(const Point *A, const Point *B, const Point *C,
 		     SDL_Surface *triangle,
 		     const Texture *U, const Texture *V, const Texture *W,
-		     const Point *normalA, const Point *normalB,
+		     const Point *normalA, 
+		     const Point *normalB,
 		     const Point *normalC)
 {
     Frame *camera = getCamera();
@@ -99,11 +237,8 @@ void projectTriangle(const Point *A, const Point *B, const Point *C,
     float depthA = scalarProduct(&camera->j, &OA);
     float depthB = scalarProduct(&camera->j, &OB);
     float depthC = scalarProduct(&camera->j, &OC);
-    int test = (depthA > nearplan) + 
-	       (depthB > nearplan) + 
-	       (depthC > nearplan);
 
-    if (test == 3) {
+    if (depthA > nearplan && depthB > nearplan && depthC > nearplan){
 	Coord a, b, c;
 	projectCoord(&OA, depthA, &a);
 	projectCoord(&OB, depthB, &b);
@@ -113,336 +248,47 @@ void projectTriangle(const Point *A, const Point *B, const Point *C,
 		     triangle,
 		     U, V, W,
 		     normalA, normalB, normalC);
-    } else if (test == 0) {
-	return;
-    } else if (test == 1) {
-	if (depthA > nearplan) {
-	    Point b, c;
-	    diffPoint(B, A, &b);
-	    diffPoint(C, A, &c);
-
-	    float kB = (nearplan - depthA) / 
-		scalarProduct(&camera->j, &b);
-	    float kC = (nearplan - depthA) / 
-		scalarProduct(&camera->j, &c);
-
-	    setPoint(&OB, 
-		     A->x + kB * b.x - camera->O.x,
-		     A->y + kB * b.y - camera->O.y,
-		     A->z + kB * b.z - camera->O.z);
-	    setPoint(&OC,
-		     A->x + kC * c.x - camera->O.x,
-		     A->y + kC * c.y - camera->O.y,
-		     A->z + kC * c.z - camera->O.z);
-
-	    Coord a, bn, cn;
-	    projectCoord(&OA, depthA, &a);
-	    projectCoord(&OB, nearplan, &bn);
-	    projectCoord(&OC, nearplan, &cn);
-
-	    Texture UV, UW;
-	    setTexture(&UV,
-		       (V->x - U->x) * kB + U->x,
-		       (V->y - U->y) * kB + U->y);
-	    setTexture(&UW,
-		       (W->x - U->x) * kC + U->x,
-		       (W->y - U->y) * kC + U->y);
-
-	    Point nAB, nAC;
-	    setPoint(&nAB,
-		     (normalB->x - normalA->x) * kB + normalA->x,
-		     (normalB->y - normalA->y) * kB + normalA->y,
-		     (normalB->z - normalA->z) * kB + normalA->z);
-	    setPoint(&nAC,
-		     (normalC->x - normalA->x) * kC + normalA->x,
-		     (normalC->y - normalA->y) * kC + normalA->y,
-		     (normalC->z - normalA->z) * kC + normalA->z);
-	    
-	    drawTriangle(&a, &bn, &cn,
-			 depthA,
-			 (depthB - depthA) * kB + depthA,
-			 (depthC - depthA) * kC + depthA,
-			 triangle,
-			 U, &UV, &UW,
-			 normalA, &nAB, &nAC);
-		
-	} else if (depthB > nearplan) {
-	    Point c, a;
-	    diffPoint(C, B, &c);
-	    diffPoint(A, B, &a);
-
-	    float kC = (nearplan - depthB) / 
-		scalarProduct(&camera->j, &c);
-	    float kA = (nearplan - depthB) / 
-		scalarProduct(&camera->j, &a);
-
-	    setPoint(&OC,
-		     B->x + kC * c.x - camera->O.x,
-		     B->y + kC * c.y - camera->O.y,
-		     B->z + kC * c.z - camera->O.z);
-	    setPoint(&OA,
-		     B->x + kA * a.x - camera->O.x,
-		     B->y + kA * a.y - camera->O.y,
-		     B->z + kA * a.z - camera->O.z);
-
-	    Coord b, cn, an;
-	    projectCoord(&OB, depthB, &b);
-	    projectCoord(&OC, nearplan, &cn);
-	    projectCoord(&OA, nearplan, &an);
-
-	    Texture VW, VU;
-	    setTexture(&VW,
-		       (W->x - V->x) * kC + V->x,
-		       (W->y - V->y) * kC + V->y);
-	    setTexture(&VU,
-		       (U->x - V->x) * kA + V->x,
-		       (U->y - V->y) * kA + V->y);
-
-	    Point nBC, nBA;
-	    setPoint(&nBC,
-		     (normalC->x - normalB->x) * kC + normalB->x,
-		     (normalC->y - normalB->y) * kC + normalB->y,
-		     (normalC->z - normalB->z) * kC + normalB->z);
-	    setPoint(&nBA,
-		     (normalA->x - normalB->x) * kA + normalB->x,
-		     (normalA->y - normalB->y) * kA + normalB->y,
-		     (normalA->z - normalB->z) * kA + normalB->z);
-	    
-	    drawTriangle(&b, &cn, &an,
-			 depthB,
-			 (depthC - depthB) * kC + depthB,
-			 (depthA - depthB) * kA + depthB,
-			 triangle,
-			 V, &VW, &VU,
-			 normalB, &nBC, &nBA);
-
-	} else if (depthC > nearplan) {
-	    Point a,b;
-	    diffPoint(A, C, &a);
-	    diffPoint(B, C, &b);
-
-	    float kA = (nearplan - depthC) / 
-		scalarProduct(&camera->j, &a);
-	    float kB = (nearplan - depthC) / 
-		scalarProduct(&camera->j, &b);
-
-	    setPoint(&OA,
-		     C->x + kA * a.x - camera->O.x,
-		     C->y + kA * a.y - camera->O.y,
-		     C->z + kA * a.z - camera->O.z);
-	    setPoint(&OB,
-		     C->x + kB * b.x - camera->O.x,
-		     C->y + kB * b.y - camera->O.y,
-		     C->z + kB * b.z - camera->O.z);
-
-	    Coord c, an, bn;
-	    projectCoord(&OC, depthC, &c);
-	    projectCoord(&OA, nearplan, &an);
-	    projectCoord(&OB, nearplan, &bn);
-
-	    Texture WU, WV;
-	    setTexture(&WU,
-		       (U->x - W->x) * kA + W->x,
-		       (U->y - W->y) * kA + W->y);
-	    setTexture(&WV,
-		       (V->x - W->x) * kB + W->x,
-		       (V->y - W->y) * kB + W->y);
-
-	    Point nCA, nCB;
-	    setPoint(&nCA,
-		     (normalA->x - normalC->x) * kA + normalC->x,
-		     (normalA->y - normalC->y) * kA + normalC->y,
-		     (normalA->z - normalC->z) * kA + normalC->z);
-	    setPoint(&nCB,
-		     (normalB->x - normalC->x) * kB + normalC->x,
-		     (normalB->y - normalC->y) * kB + normalC->y,
-		     (normalB->z - normalC->z) * kB + normalC->z);
-	    
-	    drawTriangle(&c, &an, &bn,
-			 depthC,
-			 (depthA - depthC) * kA + depthC,
-			 (depthB - depthC) * kB + depthC,
-			 triangle,
-			 W, &WU, &WV,
-			 normalC, &nCA, &nCB);
-	}
-    } else {
-	// Coord XY, XZ, Y, Z;
-	if (depthA < nearplan) {
-	    Point AB, AC;
-	    diffPoint(B, A, &AB);
-	    diffPoint(C, A, &AC);
-
-	    float kB = (nearplan - depthA) / 
-		scalarProduct(&camera->j, &AB);
-	    float kC = (nearplan - depthA) / 
-		scalarProduct(&camera->j, &AC);
-
-	    Point OpB, OpC;
-	    setPoint(&OpB,
-		     A->x + kB * AB.x - camera->O.x,
-		     A->y + kB * AB.y - camera->O.y,
-		     A->z + kB * AB.z - camera->O.z);
-	    setPoint(&OpC,
-		     A->x + kC * AC.x - camera->O.x,
-		     A->y + kC * AC.y - camera->O.y,
-		     A->z + kC * AC.z - camera->O.z);
-
-	    Coord b, c, opbn, opcn;
-	    projectCoord(&OB, depthB, &b);
-	    projectCoord(&OC, depthC, &c);
-	    projectCoord(&OpB, nearplan, &opbn);
-	    projectCoord(&OpC, nearplan, &opcn);
-
-	    Texture VU, WU;
-	    setTexture(&VU,
-		       (V->x - U->x) * kB + U->x,
-		       (V->y - U->y) * kB + U->y);
-	    setTexture(&WU,
-		       (W->x - U->x) * kC + U->x,
-		       (W->y - U->y) * kC + U->y);
-
-	    Point nAB, nAC;
-	    setPoint(&nAB,
-		     (normalB->x - normalA->x) * kB + normalA->x,
-		     (normalB->y - normalA->y) * kB + normalA->y,
-		     (normalB->z - normalA->z) * kB + normalA->z);
-	    setPoint(&nAC,
-		     (normalC->x - normalA->x) * kC + normalA->x,
-		     (normalC->y - normalA->y) * kC + normalA->y,
-		     (normalC->z - normalA->z) * kC + normalA->z);
-	    
-	    drawTriangle(&opbn, &c, &opcn,
-			 (depthB - depthA) * kB + depthA,
-			 depthC,
-			 (depthC - depthA) * kC + depthA,
-			 triangle,
-			 &VU, W, &WU,
-			 &nAB, normalC, &nAC);
-
-	    drawTriangle(&opbn, &b, &c,
-			 (depthB - depthA) * kB + depthA,
-			 depthB, depthC,
-			 triangle,
-			 &VU, V, W,
-			 &nAB, normalB, normalC);
-	} else if (depthB < nearplan) {
-	    Point BC, BA;
-	    diffPoint(C, B, &BC);
-	    diffPoint(A, B, &BA);
-
-	    float kC = (nearplan - depthB) / 
-		scalarProduct(&camera->j, &BC);
-	    float kA = (nearplan - depthB) / 
-		scalarProduct(&camera->j, &BA);
-
-	    Point OpC, OpA;
-	    setPoint(&OpC,
-		     B->x + kC * BC.x - camera->O.x,
-		     B->y + kC * BC.y - camera->O.y,
-		     B->z + kC * BC.z - camera->O.z);
-	    setPoint(&OpA,
-		     B->x + kA * BA.x - camera->O.x,
-		     B->y + kA * BA.y - camera->O.y,
-		     B->z + kA * BA.z - camera->O.z);
-
-	    Coord c, a, opcn, opan;
-	    projectCoord(&OC, depthC, &c);
-	    projectCoord(&OA, depthA, &a);
-	    projectCoord(&OpC, nearplan, &opcn);
-	    projectCoord(&OpA, nearplan, &opan);
-
-	    Texture VW, VU;
-	    setTexture(&VW,
-		       (W->x - V->x) * kC + V->x,
-		       (W->y - V->y) * kC + V->y);
-	    setTexture(&VU,
-		       (U->x - V->x) * kA + V->x,
-		       (U->y - V->y) * kA + V->y);
-
-	    Point nBC, nBA;
-	    setPoint(&nBC,
-		     (normalC->x - normalB->x) * kC + normalB->x,
-		     (normalC->y - normalB->y) * kC + normalB->y,
-		     (normalC->z - normalB->z) * kC + normalB->z);
-	    setPoint(&nBA,
-		     (normalA->x - normalB->x) * kA + normalB->x,
-		     (normalA->y - normalB->y) * kA + normalB->y,
-		     (normalA->z - normalB->z) * kA + normalB->z);
-
-	    drawTriangle(&opcn, &a, &opan,
-			 (depthC - depthB) * kC + depthB,
-			 depthA,
-			 (depthA - depthB) * kA + depthB,
-			 triangle,
-			 &VW, U, &VU,
-			 &nBC, normalA, &nBA);
-
-	    drawTriangle(&opcn, &c, &a,
-			 (depthC - depthB) * kC + depthB,
-			 depthC, depthA,
-			 triangle,
-			 &VW, W, U,
-			 &nBC, normalC, normalA);
-	} else if (depthC < nearplan) {
-	    Point CA, CB;
-	    diffPoint(A, C, &CA);
-	    diffPoint(B, C, &CB);
-
-	    float kA = (nearplan - depthC) / 
-		scalarProduct(&camera->j, &CA);
-	    float kB = (nearplan - depthC) / 
-		scalarProduct(&camera->j, &CB);
-
-	    Point OpA, OpB;
-	    setPoint(&OpA,
-		     C->x + kA * CA.x - camera->O.x,
-		     C->y + kA * CA.y - camera->O.y,
-		     C->z + kA * CA.z - camera->O.z);
-	    setPoint(&OpB,
-		     C->x + kB * CB.x - camera->O.x,
-		     C->y + kB * CB.y - camera->O.y,
-		     C->z + kB * CB.z - camera->O.z);
-
-	    Coord a, b, opan, opbn;
-
-	    projectCoord(&OA, depthA, &a);
-	    projectCoord(&OB, depthB, &b);
-	    projectCoord(&OpA, nearplan, &opan);	   
-	    projectCoord(&OpB, nearplan, &opbn);
-
-	    Texture WU, WV;
-	    setTexture(&WU,
-		       (U->x - W->x) * kA + W->x,
-		       (U->y - W->y) * kA + W->y);
-	    setTexture(&WV,
-		       (V->x - W->x) * kB + W->x,
-		       (V->y - W->y) * kB + W->y);
-
-	    Point nCA, nCB;
-	    setPoint(&nCA,
-		     (normalA->x - normalC->x) * kA + normalC->x,
-		     (normalA->y - normalC->y) * kA + normalC->y,
-		     (normalA->z - normalC->z) * kA + normalC->z);
-	    setPoint(&nCB,
-		     (normalB->x - normalC->x) * kB + normalC->x,
-		     (normalB->y - normalC->y) * kB + normalC->y,
-		     (normalB->z - normalC->z) * kB + normalC->z);
-
-	    drawTriangle(&opan, &b, &opbn,
-			 (depthA - depthC) * kA + depthC,
-			 depthB,
-			 (depthB - depthC) * kB + depthC,
-			 triangle,
-			 &WU, V, &WV,
-			 &nCA, normalB, &nCB);
-
-	    drawTriangle(&opan, &a, &b,
-			 (depthA - depthC) * kA + depthC,
-			 depthA, depthB,
-			 triangle,
-			 &WU, U, V,
-			 &nCA, normalA, normalB);
-	}
+    } else if (depthB > nearplan && depthC > nearplan){
+	cutInTwoTriangles(A, B, C,
+			  depthA, depthB, depthC,
+			  &OB, &OC,
+			  triangle,
+			  U, V, W,
+			  normalA, normalB, normalC);
+    } else if (depthC > nearplan && depthA > nearplan){
+	cutInTwoTriangles(B, C, A,
+			  depthB, depthC, depthA,
+			  &OC, &OA,
+			  triangle,
+			  V, W, U,
+			  normalB, normalC, normalA);
+    } else if (depthA > nearplan && depthB > nearplan){
+	cutInTwoTriangles(C, A, B,
+			  depthC, depthA, depthB,
+			  &OA, &OB,
+			  triangle,
+			  W, U, V,
+			  normalC, normalA, normalB);
+    } else if (depthA > nearplan){
+	cutInOneTriangle(A, B, C,
+			  depthA, depthB, depthC,
+			  &OA,
+			  triangle,
+			  U, V, W,
+			  normalA, normalB, normalC);
+    } else if (depthB > nearplan){
+	cutInOneTriangle(B, C, A,
+			  depthB, depthC, depthA,
+			  &OB,
+			  triangle,
+			  V, W, U,
+			  normalB, normalC, normalA);
+    } else if (depthC > nearplan){
+	cutInOneTriangle(C, A, B,
+			  depthC, depthA, depthB,
+			  &OC,
+			  triangle,
+			  W, U, V,
+			  normalC, normalA, normalB);
     }
 }
