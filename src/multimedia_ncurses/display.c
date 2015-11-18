ba9c955e51bd34e452eb1cf69951091f9acc34eb
Romain d'Esparbès <desparbes.romain@hotmail.fr>
2015-11-18 12:10:15 +0100

    Correction bug background

--- src/multimedia_ncurses/display.c
+++ src/multimedia_ncurses/display.c
@@ -10,6 +10,7 @@
 
 static struct {
     Color untextured;
+    int background;
     short nbColor;
     short range1;
     short range2;
@@ -63,31 +64,50 @@ static void getRGBFromId(int id, int *r, int *g, int *b)
 // 0 <= id < nbColor et 0 <= r, g et b < range1
 static int getIdFromColor(const Color *c)
 {
-    int r, g, b;
+    int r, g, b, id;
     r = c->r * (display.range1 - 1) / 255;
     g = c->g * (display.range1 - 1) / 255;
     b = c->b * (display.range1 - 1) / 255;
-    return r * display.range2 + g * display.range1 + b;
+    id = r * display.range2 + g * display.range1 + b;
+    if (id == 0)
+	return display.background;
+    else if (id == display.background)
+	return 0;
+    else
+	return id;
+}
+
+static void getCOLORFromId(int id, int *r, int *g, int *b)
+{
+    getRGBFromId(id, r, g, b);
+    getCOLORFromRGB(r, g, b);
+}
+
+static void setIdWithCOLOR(int id, int r, int g, int b)
+{
+    init_color(id, r, g, b);
+    init_pair(id, id, id);
 }
 
 static int initColor(const Color *background, const Color *untextured)
 {
     if (has_colors() == FALSE || can_change_color() == FALSE)
 	return 0;
-
+    int r, g, b;
     start_color();
     display.untextured = *untextured;
     display.range1 = getRange(min(COLOR_PAIRS, COLORS));
     display.range2 = display.range1 * display.range1;
     display.nbColor = display.range2 * display.range1;
-    for (int i = 0; i < display.nbColor; i++) {
-	int r, g, b;
-	getRGBFromId(i, &r, &g, &b);
-	getCOLORFromRGB(&r, &g, &b);
-	init_color(i, r, g, b);
-	init_pair(i, i, i);
+
+    for (int i = 1; i < display.nbColor; i++) {
+	getCOLORFromId(i, &r, &g, &b);
+	setIdWithCOLOR(i, r, g, b);
     }
-    wbkgd(stdscr, COLOR_PAIR(getIdFromColor(background)));
+    display.background = getIdFromColor(background);
+    getCOLORFromId(display.background, &r, &g, &b);
+    setIdWithCOLOR(display.background, 0, 0, 0);
+    setIdWithCOLOR(0, r, g, b);
     return 1;
 }
 
